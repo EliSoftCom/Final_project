ba14aad1b16fc4d5a6f2722558f8777909aff4c6
Cosmmoss <pavelandreevich16@gmail.com>
2024-06-22 17:27:35 +0300

    Created parser Drom

---
+++ get_data.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+
+def get_html(url):
+    try:
+        result = requests.get(url)
+        result.raise_for_status()
+        return result.text
+    except(requests.RequestException, ValueError):
+        print('Сетевая ошибка')
+        return False
+
+def get_data():
+    html = get_html('https://auto.drom.ru/ford/mondeo/')
+    if html:
+        soup = BeautifulSoup(html, 'html.parser')
+        all_data = soup.find('div', class_='css-1nvf6xk eojktn00').find('div').findAll('a')
+        result_data = []
+        for data in all_data:
+            name = data.find('div', class_='css-16kqa8y e3f4v4l2').contents[0]
+            url = data.get('href')
+            price = data.find('span', class_='css-46itwz').find('span').text
+            description = data.find('div', class_='css-1fe6w6s').get_text()
+            result_data.append({
+                'name': name,
+                'url': url,
+                'price': price,
+                'description': description
+            })
+        return result_data    
+    return False
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask import render_template
+from get_data import get_data
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    data_parser = get_data()
+    return render_template('index.html', data_parser=data_parser)
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+
