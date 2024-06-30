f2f8e2e93327b4a99b1c5b4a1f01e22049a30110
Cosmmoss <pavelandreevich16@gmail.com>
2024-06-30 12:54:04 +0300

    Удалил из репозитория файл webapp/config.py

--- webapp/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'data_parser.db')
-SECRET_KEY = "asd5dfv12dfg5gf53&bnbnb^2vbvgls"
